b8c005e86c3e1fe0e91af98dbc7e4ccc6426f486
Sylvia Engmann <sylvia.engmann@ashesi.edu.gh>
2016-10-21 14:06:02 +0000

    Created a geolocation app
    Added the files

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+﻿(function() {
+	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+	function onDeviceReady() {
+		  
+		document.getElementById("geolocationbtn").addEventListener("click", function(){
+  			navigator.geolocation.getCurrentPosition(onSuccess,onError,
+  												{ timeout: 30000,enableHighAccuracy: true 
+  		});
+  		});
+  		var watchID = navigator.geolocation.watchPosition(onWatchSuccess,onWatchError,{
+  		    timeout: 30000,enableHighAccuracy: true 
+  		});	
+
+		document.getElementById("clearWatchbtn").addEventListener("click", function() {
+			navigator.geolocation.clearWatch(watchID);
+		});
+
+  		};
+
+		
+  	var onSuccess = function(position){
+  		alert('Latitude: ' + position.coords.latitude + '\n' + 'Longitude: ' + position.coords.longitude + '\n');
+  	}
+
+	 function onError(error){
+		alert('code:' + error.code +'\n'+
+				'message' +error.message + '\n');
+	}
+
+	function onWatchSuccess(position){
+		var element = document.getElementById('geolocation');
+		element.innerHTML = 'Latitude: '+position.coords.latitude +'<br/'+'Longitude:' +position.coords.longitude +'<br/'+'<hr />' element.innerHTML;
+	}
+	function onWatchError(error){
+		alert('code:' + error.code +'\n'+
+				'message' +error.message + '\n');
+	}
+
+})();
