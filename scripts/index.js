abbec330479e4138bdbf0abb044bac012ff6086f
Sylvia Engmann <sylvia.engmann@ashesi.edu.gh>
2016-10-23 22:09:36 +0000

    Restructured the whole application
    Changed the positions of the images, as well as added some missing
    libraries

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,41 @@
 ﻿(function() {
 	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
-	function onDeviceReady() {		  
-		document.getElementById("geolocationbtn").addEventListener("click", function(){
-  			navigator.geolocation.getCurrentPosition(onSuccess,onError,
-  												{ enableHighAccuracy: true 
-  		});
+	function onDeviceReady() {
+		alert('My device is ready');
+		
+		document.getElementById("geolocationbtn").addEventListener("click", function() {
+			navigator.geolocation.getCurrentPosition(onSuccess, onError, {
+				enableHighAccuracy : true
+			});
 
-  		});
+		});
 
-  		var watchID = navigator.geolocation.watchPosition(onWatchSuccess,onWatchError,{
-  		    timeout: 30000 
-  		});	
+		var watchID = navigator.geolocation.watchPosition(onWatchSuccess, onWatchError, {
+			timeout : 30000
+		});
 
 		document.getElementById("clearWatchBtn").addEventListener("click", function() {
 			navigator.geolocation.clearWatch(watchID);
 		});
 
-  		};
+	};
 
-		
-  	var onSuccess = function(position){
-  		alert('Latitude: ' + position.coords.latitude + '\n' + 'Longitude: ' + position.coords.longitude + '\n');
-  	};
+	var onSuccess = function(position) {
+		alert('Latitude: ' + position.coords.latitude + '\n' + 'Longitude: ' + position.coords.longitude + '\n');
+	};
 
-	 function onError(error){
-		alert('code:' + error.code +'\n'+
-				'message' +error.message + '\n');
-	}
+	function onError(error) {
+		alert('code:' + error.code + '\n' + 'message' + error.message + '\n');
+	};
 
-	function onWatchSuccess(position){
+	function onWatchSuccess(position) {
 		var element = document.getElementById('geolocation');
-		element.innerHTML = 'Latitude: '+position.coords.latitude +'<br/'+'Longitude:' +position.coords.longitude +'<br/'+'<hr />' +element.innerHTML;
-	}
+		element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br/' + 'Longitude:' + position.coords.longitude + '<br/' + '<hr />' + element.innerHTML;
+	};
 
-	function onWatchError(error){
-		alert('code:' + error.code +'\n'+
-				'message' +error.message + '\n');
-	}
+	function onWatchError(error) {
+		alert('code:' + error.code + '\n' + 'message' + error.message + '\n');
+	};
 
 })();
